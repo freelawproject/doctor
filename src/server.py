bf393872eaddb2b667931eb07832867dddd282a8
vvilliam <wpalin@gmail.com>
2020-09-14 12:02:36 -0400

    feat(server.py): Init extract_content and convert_audio_file
    Add methods to convert documents and audio

--- src/server.py
+++ src/server.py
@@ -1,4 +1,18 @@
-from flask import Flask
+from tempfile import NamedTemporaryFile
+
+from flask import Flask, request, send_file
+
+from src.utils.audio import convert_mp3
+from src.utils.tasks import (
+    extract_from_docx,
+    extract_from_doc,
+    extract_from_wpd,
+    extract_from_html,
+    extract_from_txt,
+    make_pdftotext_process,
+    extract_from_pdf,
+    rasterize_pdf,
+)
 
 app = Flask(__name__)
 
@@ -11,6 +25,70 @@ def sanity_check():
     :type: dict
     """
     return {"success": True, "msg": "Docker container running."}
+
+
+@app.route("/extract_doc_content", methods=["POST"])
+def extract_content():
+    """Pass documents to extract content
+
+    :return: Content of documents
+    :type: str
+    """
+
+    f = request.files["file"]
+    do_ocr = request.args.get("do_ocr", default=False)
+    if do_ocr == "True":
+        do_ocr = True
+
+    extension = f.filename.split(".")[-1]
+    with NamedTemporaryFile(suffix=".%s" % extension) as tmp:
+        f.save(tmp.name)
+
+        if extension == "pdf":
+            process = make_pdftotext_process(tmp.name)
+            content, err = process.communicate()
+            if do_ocr and len(content.strip()) == 0:
+                with NamedTemporaryFile(suffix=".tiff") as tmp_tiff:
+                    out, err, returncode = rasterize_pdf(
+                        tmp.name, tmp_tiff.name
+                    )
+                    content = extract_from_pdf(tmp_tiff)
+                    if content == "":
+                        content = "Unable to extract document content."
+                        return content, False
+                    return content, True
+            else:
+                if len(content.strip()) == 0:
+                    return "", False
+                return content.strip(), True
+
+        elif extension == "doc":
+            return extract_from_doc(tmp.name)
+        elif extension == "docx":
+            return extract_from_docx(tmp.name)
+        elif extension == "html":
+            return extract_from_html(tmp.name)
+        elif extension == "txt":
+            return extract_from_txt(tmp.name)
+        elif extension == "wpd":
+            return extract_from_wpd(tmp.name)
+        # elif extension == "rtf": return extract_from_rtf(tmp.name) # to do
+
+        else:
+            print(
+                "*****Unable to extract content due to unknown extension: %s "
+                "on opinion: %s****" % (extension, "opinion_pk")
+            )
+            return
+
+
+@app.route("/convert_audio_file", methods=["POST"])
+def convert_audio_file():
+    f = request.files["file"]
+    with NamedTemporaryFile() as tmp:
+        f.save(tmp.name)
+        converted_file = convert_mp3(tmp.name)
+        return send_file(converted_file)
 
 
 if __name__ == "__main__":
