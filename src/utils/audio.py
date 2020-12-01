b68fd26ea90d4c264657f496bffd3a38f16da2c8
vvilliam <wpalin@gmail.com>
2020-12-01 13:45:54 -0500

    refactor(set_mp3_meta_data): Update method to use new dictionary

--- src/utils/audio.py
+++ src/utils/audio.py
@@ -68,14 +68,15 @@ def convert_mp3(audio_bytes: ByteString, tmp_path: AnyStr) -> NoReturn:
     ffmpeg_cmd.communicate(audio_bytes)
 
 
-def set_mp3_meta_data(audio_obj, mp3_path):
-    """Sets the meta data on the mp3 file to good values.
+def set_mp3_meta_data(
+    audio_data: Dict, mp3_path: AnyStr
+) -> eyed3.core.AudioFile:
+    """Set the metadata in audio_data to an mp3 at path.
 
-    :param audio_obj: an Audio object to clean up.
-    :param mp3_path: the path to the mp3 to be converted.
+    :param audio_data: The new metadata to embed in the mp3.
+    :param mp3_path: The path to the mp3 to be converted.
+    :return: Eyed3 audio file object
     """
-
-    court = audio_obj.docket.court
 
     # Load the file, delete the old tags and create a new one.
     audio_file = eyed3.load(mp3_path)
@@ -86,32 +87,29 @@ def set_mp3_meta_data(audio_obj, mp3_path):
         preserve_file_time=False,
     )
     audio_file.initTag()
-    audio_file.tag.title = best_case_name(audio_obj)
-    date_argued = datetime.datetime.strptime(
-        audio_obj.docket.date_argued, "%Y-%m-%d"
+    audio_file.tag.title = best_case_name(audio_data)
+    date_argued = audio_data["date_argued"]
+    docket_number = audio_data["docket_number"]
+    audio_file.tag.album = (
+        f"{audio_data['court_full_name']}, {audio_data['date_argued_year']}"
     )
-    audio_file.tag.album = "{court}, {year}".format(
-        court=court.full_name, year=date_argued.year
-    )
-    audio_file.tag.artist = court.full_name
-    audio_file.tag.artist_url = court.url
-    audio_file.tag.audio_source_url = audio_obj.download_url
+    audio_file.tag.artist = audio_data["court_full_name"]
+    audio_file.tag.artist_url = audio_data["court_url"]
+    audio_file.tag.audio_source_url = audio_data["download_url"]
+
     audio_file.tag.comments.set(
-        "Argued: {date_argued}. Docket number: {docket_number}".format(
-            date_argued=date_argued,
-            docket_number=audio_obj.docket.docket_number,
-        )
+        f"Argued: {date_argued}. Docket number: {docket_number}"
     )
     audio_file.tag.genre = "Speech"
     audio_file.tag.publisher = "Free Law Project"
     audio_file.tag.publisher_url = "https://free.law"
-    audio_file.tag.recording_date = audio_obj.docket.date_argued
+    audio_file.tag.recording_date = date_argued
 
     # Add images to the mp3. If it has a seal, use that for the Front Cover
     # and use the FLP logo for the Publisher Logo. If it lacks a seal, use the
     # Publisher logo for both the front cover and the Publisher logo.
     try:
-        has_seal = seals_data[court.pk]["has_seal"]
+        has_seal = seals_data[audio_data["court_pk"]]["has_seal"]
     except AttributeError:
         # Unknown court in Seal Rookery.
         has_seal = False
@@ -125,10 +123,14 @@ def set_mp3_meta_data(audio_obj, mp3_path):
     ]
     if has_seal:
         with open(
-            os.path.join(seals_root, "512", "%s.png" % court.pk), "rb"
+            os.path.join(seals_root, "512", f"{audio_data['court_pk']}.png"),
+            "rb",
         ) as f:
             audio_file.tag.images.set(
-                3, f.read(), "image/png", "Seal for %s" % court.short_name
+                3,
+                f.read(),
+                "image/png",
+                "Seal for %s" % audio_data["court_short_name"],
             )
         flp_image_frames.remove(3)
 
